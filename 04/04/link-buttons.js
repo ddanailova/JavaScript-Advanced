73aa1fe1c36e632174b45f355bccb331df68b6b2
Daniela Danailova <d.k.danailova@gmail.com>
2018-10-19 15:18:57 +0300

    Delete link-buttons.js

--- 04/04/link-buttons.js
+++
@@ -1,6 +0,0 @@
-function attachEvents() {
-    $('.button').on('click', function(){
-        $('.button').toArray().forEach(btn => $(btn).removeClass('selected'));
-       $(this).addClass('selected');
-    })
-}
